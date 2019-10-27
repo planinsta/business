4ea6ab3757a7a02d85595eb83ddbc8ba0e06969e
ilievlad73 <ilievlad73@gmail.com>
2019-10-28 00:27:36 +0200

    Updates

---
+++ precache-manifest.d802c90c73444f8ce9f0dc0b6c354b7f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8191ab8d019d0fe390fd4ed8fc44931",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "b911221eab6ab3e13d5d",
+    "url": "/home/static/css/main.d0cd2490.chunk.css"
+  },
+  {
+    "revision": "f290ba0f8d8d2617e2e2",
+    "url": "/home/static/js/2.d8a2bfa4.chunk.js"
+  },
+  {
+    "revision": "b911221eab6ab3e13d5d",
+    "url": "/home/static/js/main.7c3355b8.chunk.js"
+  },
+  {
+    "revision": "61928bcb4b4102fdc79c",
+    "url": "/home/static/js/runtime-main.9914e1ff.js"
+  },
+  {
+    "revision": "65e17c67f845ef6b91cb7e2804d31baa",
+    "url": "/home/static/media/approval.65e17c67.svg"
+  },
+  {
+    "revision": "93b79b23cf4f7bbbadf3717c6ea5425d",
+    "url": "/home/static/media/calendar.93b79b23.svg"
+  },
+  {
+    "revision": "9b77d7c8a72997e87d88f61795ca813f",
+    "url": "/home/static/media/calendar.9b77d7c8.svg"
+  },
+  {
+    "revision": "a97950e40de7048579ca40ea746b3068",
+    "url": "/home/static/media/contact_us.a97950e4.svg"
+  },
+  {
+    "revision": "5806401a68ccf0f5cdea43a1628915de",
+    "url": "/home/static/media/favorite.5806401a.svg"
+  },
+  {
+    "revision": "f93e5a6c221788c41a4d58488ee36ab1",
+    "url": "/home/static/media/favorite.f93e5a6c.svg"
+  },
+  {
+    "revision": "5f44fc68478906e4af72424e5b2994dc",
+    "url": "/home/static/media/history.5f44fc68.svg"
+  },
+  {
+    "revision": "fc55566c1f7ea2a70e35d0d7b900cb43",
+    "url": "/home/static/media/online-store.fc55566c.svg"
+  },
+  {
+    "revision": "6d965be42b4f867e54896e50e3237ce1",
+    "url": "/home/static/media/profile.6d965be4.svg"
+  },
+  {
+    "revision": "bf967b40c4435af555eedf3202bc1dd0",
+    "url": "/home/static/media/profile.bf967b40.svg"
+  },
+  {
+    "revision": "fd962f9ec9b4cb655b5237e8728fa4fb",
+    "url": "/home/static/media/profile.fd962f9e.svg"
+  },
+  {
+    "revision": "41623a4c33fe92c9b47faf1d77af9ce4",
+    "url": "/home/static/media/review.41623a4c.svg"
+  },
+  {
+    "revision": "d67a5f72a6378bec90d2d4789e2cfe4e",
+    "url": "/home/static/media/review.d67a5f72.svg"
+  },
+  {
+    "revision": "3417a6c5de9f26b5002769c3f3e298d3",
+    "url": "/home/static/media/salon.3417a6c5.jpg"
+  },
+  {
+    "revision": "01e100b43cadea3e5a89bdfc6c89de21",
+    "url": "/home/static/media/search.01e100b4.svg"
+  },
+  {
+    "revision": "ae78b40cddb33fa62f59fe7708352c1f",
+    "url": "/home/static/media/search.ae78b40c.svg"
+  },
+  {
+    "revision": "293a39bb2cb0894d8fc143eaa7d57487",
+    "url": "/home/static/media/solution.293a39bb.svg"
+  },
+  {
+    "revision": "c9f25002173db23dfe669a77d3a13dad",
+    "url": "/home/static/media/solution.c9f25002.svg"
+  },
+  {
+    "revision": "b0ed1c77f66e3bf36367b3b297b510cb",
+    "url": "/home/static/media/user_approval.b0ed1c77.svg"
+  }
+]);
